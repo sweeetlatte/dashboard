e068eea9252bf377b82b28d6967f909a0385f58a
sweeetlatte <thuylinh271055@gmail.com>
2022-11-05 17:06:06 +0700

    feat: avatar dropdown

--- src/components/Header/Avatar/index.tsx
+++ src/components/Header/Avatar/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import * as Icon from 'react-feather';
 
 import avatar from '../../../assets/images/avatar.jpg';
+import { toggleMask } from '../../../redux/actions';
+import { useAppDispatch, useAppSelector } from '../../../redux/store';
 import style from './styles.module.scss';
 
 export default function Avatar() {
@@ -26,14 +28,18 @@ export default function Avatar() {
     },
   ];
   const [showDropdown, setShowDropdown] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const maskOn = useAppSelector((state) => state.mask);
 
   return (
     <div className={style.userWrap}>
       <div
         className={style.avatarWrap}
         onClick={() => {
-          if (showDropdown === false) setShowDropdown(true);
-          else setShowDropdown(false);
+          if (showDropdown === false) {
+            setShowDropdown(true);
+            dispatch(toggleMask());
+          } else setShowDropdown(false);
         }}
       >
         <div className={style.avatar}>
@@ -44,35 +50,37 @@ export default function Avatar() {
         </p>
         <Icon.ChevronDown size={20} strokeWidth={1} />
       </div>
-      {showDropdown ? (
-        <div className={style.avatarDropdown}>
-          <p className={style.title}>User Profile</p>
-          <div className={style.info}>
-            <div className={style.avatar}>
-              <img src={avatar} alt='' />
-            </div>
-            <div className={style.textGroup}>
-              <div className={style.title}>Latte</div>
-              <p>Administrator</p>
-              <div className={style.email}>
-                <Icon.Mail size={18} />
-                <p>latte@coffee.com</p>
+      {showDropdown && maskOn ? (
+        <>
+          <div className={style.avatarDropdown}>
+            <p className={style.title}>User Profile</p>
+            <div className={style.info}>
+              <div className={style.avatar}>
+                <img src={avatar} alt='' />
               </div>
-            </div>
-          </div>
-          <div className={style.detail}>
-            {details.map((detail) => (
-              <div key={detail.id} className={style.item}>
-                <div className={style.icon}>{detail.icon}</div>
-                <div className={style.text}>
-                  <p>{detail.text}</p>
-                  <p className={style.subtext}>{detail.subtext}</p>
+              <div className={style.textGroup}>
+                <div className={style.title}>Latte</div>
+                <p>Administrator</p>
+                <div className={style.email}>
+                  <Icon.Mail size={18} />
+                  <p>latte@coffee.com</p>
                 </div>
               </div>
-            ))}
+            </div>
+            <div className={style.detail}>
+              {details.map((detail) => (
+                <div key={detail.id} className={style.item}>
+                  <div className={style.icon}>{detail.icon}</div>
+                  <div className={style.text}>
+                    <p>{detail.text}</p>
+                    <p className={style.subtext}>{detail.subtext}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <button className='button'>Logout</button>
           </div>
-          <button className='button'>Logout</button>
-        </div>
+        </>
       ) : (
         <></>
       )}
