deed3879ac1e8bab721930fff4389d0e159b730a
sweeetlatte <thuylinh271055@gmail.com>
2022-12-20 17:22:42 +0700

    feat: empty pages

---
+++ src/components/CardHello/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function CardHello() {
+  return <div>CardHello</div>;
+}
--- src/pages/Analytical/index.tsx
+++ src/pages/Analytical/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// import CardHello from '../../components/CardHello';
+
 export default function Analytical() {
-  return <div>Analytical</div>;
+  return (
+    <>
+      <div>Analytical</div>
+      {/* <CardHello /> */}
+    </>
+  );
 }
