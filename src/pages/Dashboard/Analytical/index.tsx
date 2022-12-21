55377329b652da5e64ee15cbae5c8b94d090d9ff
sweeetlatte <thuylinh271055@gmail.com>
2022-12-21 17:12:59 +0700

    ---

--- src/pages/Dashboard/Analytical/index.tsx
+++ src/pages/Dashboard/Analytical/index.tsx
@@ -1,9 +1,14 @@
 import CardHello from '../../../components/CardHello';
+import styles from './styles.module.scss';
 
 export default function Analytical() {
   return (
     <>
-      <CardHello />
+      <div className={styles.analytical1}>
+        <CardHello />
+        <CardHello />
+        <CardHello />
+      </div>
     </>
   );
 }
