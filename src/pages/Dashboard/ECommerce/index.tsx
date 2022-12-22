e96464b087dd077c6c35aeaa67ea63a715d16322
sweeetlatte <thuylinh271055@gmail.com>
2022-12-22 10:57:10 +0700

    [feat][ecom] divide layout

--- src/pages/Dashboard/ECommerce/index.tsx
+++ src/pages/Dashboard/ECommerce/index.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import styles from './styles.module.scss';
 
 export default function ECommerce() {
-  return <div>eCommerce</div>;
+  return (
+    <div className={styles.ecomContainer}>
+      <div className={styles.ecom1}>
+        <div className={styles.ecom1_leftComponent}>left</div>
+        <div className={styles.ecom1_rightComponent}>right</div>
+      </div>
+      <div className={styles.ecom2}>
+        <div className={styles.ecom2_leftComponent}>left</div>
+        <div className={styles.ecom2_rightContainer}>
+          <div className={styles.ecom2_topComponent}>top</div>
+          <div className={styles.ecom2_bottomComponent}>bottom</div>
+        </div>
+      </div>
+      <div className={styles.ecom3}>
+        <div className={styles.ecom3_leftComponent}>left</div>
+        <div className={styles.ecom3_rightComponent}>right</div>
+      </div>
+      <div className={styles.ecom4}>
+        <div>left</div>
+        <div>middle</div>
+        <div>right</div>
+      </div>
+    </div>
+  );
 }
