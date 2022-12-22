2ff6f1c5575d60832d78de7859a49bbbb9cf3e79
sweeetlatte <thuylinh271055@gmail.com>
2022-12-22 09:41:38 +0700

    [style][analytical] dividing layout

--- src/pages/Dashboard/Analytical/index.tsx
+++ src/pages/Dashboard/Analytical/index.tsx
@@ -1,14 +1,27 @@
-import CardHello from '../../../components/CardHello';
 import styles from './styles.module.scss';
 
 export default function Analytical() {
   return (
-    <>
+    <div className={styles.analyticalContainer}>
       <div className={styles.analytical1}>
-        <CardHello />
-        <CardHello />
-        <CardHello />
+        <div className={styles.analytical1_leftContainer}>
+          <div className={styles.analytical1_topComponent}>up</div>
+          <div className={styles.analytical1_bottomComponent}>
+            <div>left</div>
+            <div>right</div>
+          </div>
+        </div>
+        <div className={styles.analytical1_rightComponent}>right</div>
       </div>
-    </>
+      <div className={styles.analytical2}>
+        <div className={styles.analytical2_leftComponent}>left</div>
+        <div className={styles.analytical2_rightComponent}>right</div>
+      </div>
+      <div className={styles.analytical3}>
+        <div className={styles.analytical3_leftComponent}>left</div>
+        <div className={styles.analytical3_middleComponent}>middle</div>
+        <div className={styles.analytical3_rightComponent}>right</div>
+      </div>
+    </div>
   );
 }
