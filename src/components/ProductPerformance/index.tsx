6369b834916751b0a737c38a33517c026295eb6e
sweeetlatte <thuylinh271055@gmail.com>
2022-12-23 09:12:13 +0700

    [feat][style][product performance] load data

--- src/components/ProductPerformance/index.tsx
+++ src/components/ProductPerformance/index.tsx
@@ -1,3 +1,4 @@
+import avatar from '../../assets/images/avatar.jpg';
 import CustomSelectBox from '../CustomSelectBox';
 import styles from './styles.module.scss';
 
@@ -17,6 +18,36 @@ export default function ProductPerformance() {
     },
   ];
 
+  const tableData = [
+    {
+      id: 0,
+      avatar: avatar,
+      name: 'Latte',
+      job: 'Web Developer',
+      task: 'New Theme',
+      priority: 'High',
+      budget: '$24.4k',
+    },
+    {
+      id: 1,
+      avatar: avatar,
+      name: 'Latte',
+      job: 'Web Developer',
+      task: 'New Theme',
+      priority: 'Medium',
+      budget: '$24.4k',
+    },
+    {
+      id: 2,
+      avatar: avatar,
+      name: 'Latte',
+      job: 'Web Developer',
+      task: 'New Theme',
+      priority: 'Low',
+      budget: '$24.4k',
+    },
+  ];
+
   return (
     <>
       <div className={styles.title}>
@@ -33,35 +64,40 @@ export default function ProductPerformance() {
       <div className={styles.table}>
         <table>
           <tr>
-            <th style={{ width: '42%' }}>Assigned</th>
-            <th style={{ width: '28%' }}>Name</th>
+            <th style={{ width: '40%' }}>Assigned</th>
+            <th style={{ width: '30%' }}>Task</th>
             <th style={{ width: '18%' }}>Priority</th>
             <th style={{ width: '12%', textAlign: 'right' }}>Budget</th>
           </tr>
-          <tr>
-            <td>
-              <div>avatar</div>
-              <div className={styles.info}>
-                <div>name</div>
-                <div>job</div>
-              </div>
-            </td>
-            <td>Maria Anders</td>
-            <td>Moderateee</td>
-            <td>$24.5k</td>
-          </tr>
-          <tr>
-            <td>
-              <div>avatar</div>
-              <div>
-                <div>name</div>
-                <div>job</div>
-              </div>
-            </td>
-            <td>Maria Anders</td>
-            <td>Moderateee</td>
-            <td>$24.5k</td>
-          </tr>
+          {tableData &&
+            tableData.map((row) => (
+              <tr key={row.id}>
+                <td>
+                  <div className={styles.avatar}>
+                    <img src={row.avatar} alt='' width={'100%'} />
+                  </div>
+                  <div className={styles.info}>
+                    <div className={styles.name}>{row.name}</div>
+                    <div className={styles.job}>{row.job}</div>
+                  </div>
+                </td>
+                <td>{row.task}</td>
+                <td>
+                  <div
+                    className={
+                      row.priority === 'Low'
+                        ? styles.low
+                        : row.priority === 'Medium'
+                        ? styles.medium
+                        : styles.high
+                    }
+                  >
+                    {row.priority}
+                  </div>
+                </td>
+                <td>{row.budget}</td>
+              </tr>
+            ))}
         </table>
       </div>
     </>
