6205b08baff0889dbb5e8724cdcfa8deb2049cc8
sweeetlatte <thuylinh271055@gmail.com>
2022-12-24 23:42:48 +0700

    [feat][monthly sales] chart

--- src/components/MonthlySales/index.tsx
+++ src/components/MonthlySales/index.tsx
@@ -1,26 +1,58 @@
-// import ReactApexChart from 'react-apexcharts';
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  PointElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 import * as Icon from 'react-feather';
 
 import styles from './styles.module.scss';
 
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+  },
+  scales: {
+    x: {
+      display: false,
+    },
+    y: {
+      display: false,
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [1, 5, 2, 9, 4, 3, 7],
+      borderColor: '#cec266',
+      backgroundColor: 'white',
+    },
+  ],
+};
+
 export default function MonthlySales() {
-  //   const options = {
-  //     dataLabels: {
-  //       enabled: false,
-  //     },
-  //     stroke: {
-  //       curve: 'smooth',
-  //       width: 1,
-  //     },
-  //   };
-
-  //   const series = [
-  //     {
-  //       name: 'series1',
-  //       data: [32, 48, 27, 50, 42, 112, 100],
-  //     },
-  //   ];
-
   return (
     <div className={styles.monthlySalesContainer}>
       <div className={styles.title}>
@@ -33,7 +65,7 @@ export default function MonthlySales() {
         </div>
       </div>
       <div>
-        {/* <ReactApexChart options={options} series={series} type='area' height={150} /> */}
+        <Line options={options} data={data} />
       </div>
     </div>
   );
