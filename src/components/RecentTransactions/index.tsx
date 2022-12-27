545953827db3d8d3bc42cff4eb73276f63d18899
sweeetlatte <thuylinh271055@gmail.com>
2022-12-27 15:23:29 +0700

    [feat][recent transactions] format currency

---
+++ src/components/RecentTransactions/index.tsx
@@ -0,0 +1,73 @@
+import * as Icon from 'react-feather';
+
+import styles from './styles.module.scss';
+
+export default function RecentTransactions() {
+  const transactions = [
+    {
+      id: 0,
+      icon: <Icon.DollarSign />,
+      title: 'PayPal Transfer',
+      subtitle: 'Money added',
+      change: 350,
+    },
+    {
+      id: 1,
+      icon: <Icon.Shield />,
+      title: 'Wallet',
+      subtitle: 'Bill payment',
+      change: -560,
+    },
+    {
+      id: 2,
+      icon: <Icon.CreditCard />,
+      title: 'Credit Card',
+      subtitle: 'Money reversed',
+      change: 350,
+    },
+    {
+      id: 3,
+      icon: <Icon.Check />,
+      title: 'Bank Transfer',
+      subtitle: 'Money added',
+      change: 350,
+    },
+    {
+      id: 4,
+      icon: <Icon.RefreshCw />,
+      title: 'Refund',
+      subtitle: 'Payment sent',
+      change: -50,
+    },
+  ];
+
+  return (
+    <>
+      <div className={styles.title}>Recent Transactions</div>
+      <div>
+        {transactions.map((transaction) => (
+          <div key={transaction.id} className={styles.row}>
+            <div className={styles.titleContainer}>
+              <div>{transaction.icon}</div>
+              <div>
+                <div className={styles.title}>{transaction.title}</div>
+                <div className={styles.subtitle}>{transaction.subtitle}</div>
+              </div>
+            </div>
+            {transaction.change > 0 ? (
+              <div className={styles.up}>+${transaction.change}</div>
+            ) : (
+              <div className={styles.down}>
+                {Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                }).format(transaction.change)}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div>Bottom</div>
+    </>
+  );
+}
