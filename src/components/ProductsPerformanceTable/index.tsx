83b4b0e9f5c662e26d1d8c6b5905d22386c727fd
sweeetlatte <thuylinh271055@gmail.com>
2022-12-28 08:45:31 +0700

    [feat][products performance table] td 2

--- src/components/ProductsPerformanceTable/index.tsx
+++ src/components/ProductsPerformanceTable/index.tsx
@@ -43,45 +43,60 @@ export default function ProductsPerformanceTable() {
     <>
       <div className={styles.title}>Products Performance</div>
       <table>
-        <tr>
-          <th style={{ width: '59%' }}></th>
-          <th style={{ width: '18%' }}></th>
-          <th style={{ width: '18%' }}></th>
-          <th style={{ width: '5%' }}></th>
-        </tr>
-        {tableData &&
-          tableData.map((row) => (
-            <tr key={row.id}>
-              <td>
-                <div className={styles.imgContainer}>
-                  <img src={row.img} alt='' />
-                </div>
-                <div className={styles.textContainer}>
-                  <p className={styles.name}>{row.name}</p>
-                  <p className={styles.ingres}>{row.ingres.join(', ')}</p>
-                </div>
-              </td>
-              <td>
-                {row.sold >= 90 ? (
-                  <p>Excellent</p>
-                ) : row.sold < 90 && row.sold >= 80 ? (
-                  <p>Good</p>
-                ) : row.sold < 80 && row.sold >= 50 ? (
-                  <p>Average</p>
-                ) : (
-                  <p>Poor</p>
-                )}
-                <span className={styles.fullBar}>
-                  <span className={styles.progressBar}></span>
-                </span>
-                <p className={styles.sold}>{row.sold}% sold</p>
-              </td>
-              <td>earning</td>
-              <td>
-                <Icon.Trash size={16} />
-              </td>
-            </tr>
-          ))}
+        <thead>
+          <tr>
+            <th style={{ width: '59%' }}></th>
+            <th style={{ width: '18%' }}></th>
+            <th style={{ width: '18%' }}></th>
+            <th style={{ width: '5%' }}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData &&
+            tableData.map((row) => (
+              <tr key={row.id}>
+                <td>
+                  <div className={styles.imgContainer}>
+                    <img src={row.img} alt='' />
+                  </div>
+                  <div className={styles.textContainer}>
+                    <p className={styles.name}>{row.name}</p>
+                    <p className={styles.ingres}>{row.ingres.join(', ')}</p>
+                  </div>
+                </td>
+                <td>
+                  {row.sold >= 90 ? (
+                    <p>Excellent</p>
+                  ) : row.sold < 90 && row.sold >= 80 ? (
+                    <p>Good</p>
+                  ) : row.sold < 80 && row.sold >= 50 ? (
+                    <p>Average</p>
+                  ) : (
+                    <p>Poor</p>
+                  )}
+                  <span className={styles.fullBar}>
+                    <span
+                      style={{ width: `${row.sold}px` }}
+                      className={
+                        row.sold >= 90
+                          ? styles.progressBar_excellent
+                          : row.sold < 90 && row.sold >= 80
+                          ? styles.progressBar_good
+                          : row.sold < 80 && row.sold >= 50
+                          ? styles.progressBar_average
+                          : styles.progressBar_poor
+                      }
+                    ></span>
+                  </span>
+                  <p className={styles.sold}>{row.sold}% sold</p>
+                </td>
+                <td>earning</td>
+                <td>
+                  <Icon.Trash size={16} />
+                </td>
+              </tr>
+            ))}
+        </tbody>
       </table>
     </>
   );
