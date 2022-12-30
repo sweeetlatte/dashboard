539f30f1184446bb04117f263ce0a599dde756f5
sweeetlatte <thuylinh271055@gmail.com>
2022-12-30 14:03:58 +0700

    [fix] avatar dropdown

--- src/components/Header/Avatar/index.tsx
+++ src/components/Header/Avatar/index.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import * as Icon from 'react-feather';
 
 import avatar from '../../../assets/images/avatar.jpg';
-import { toggleMask } from '../../../redux/actions';
-import { useAppDispatch, useAppSelector } from '../../../redux/store';
-import style from './styles.module.scss';
+import useComponentVisible from '../../../utils/hooks/useComponentVisible';
+import styles from './styles.module.scss';
 
 export default function Avatar() {
+  const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
   const details = [
     {
       id: 1,
@@ -27,28 +29,24 @@ export default function Avatar() {
       subtext: 'To-do & Daily Tasks',
     },
   ];
-  const [showDropdown, setShowDropdown] = useState<boolean>(false);
-  const dispatch = useAppDispatch();
-  const maskOn = useAppSelector((state) => state.mask);
 
   return (
-    <div className={style.userWrap}>
+    <div ref={ref} className={styles.userWrap}>
       <div
-        className={style.avatarWrap}
+        className={styles.avatarWrap}
         onClick={() => {
-          if (showDropdown === false) {
+          if (showDropdown === false || isComponentVisible === false) {
             setShowDropdown(true);
-            dispatch(toggleMask());
-            console.log('set true');
+            setIsComponentVisible(true);
           } else {
-            console.log('start timeout');
             setTimeout(() => {
-              dispatch(toggleMask());
+              setShowDropdown(false);
+              setIsComponentVisible(false);
             }, 500);
           }
         }}
       >
-        <div className={style.avatar}>
+        <div className={styles.avatar}>
           <img src={avatar} alt='' />
         </div>
         <p>
@@ -56,35 +54,35 @@ export default function Avatar() {
         </p>
         <Icon.ChevronDown size={20} strokeWidth={1} />
       </div>
-      {showDropdown && maskOn ? (
+      {showDropdown && isComponentVisible ? (
         <>
-          <div className={style.avatarDropdown}>
-            <p className={style.title}>User Profile</p>
-            <div className={style.info}>
-              <div className={style.avatar}>
+          <div className={styles.avatarDropdown}>
+            <p className={styles.title}>User Profile</p>
+            <div className={styles.info}>
+              <div className={styles.avatar}>
                 <img src={avatar} alt='' />
               </div>
-              <div className={style.textGroup}>
-                <div className={style.title}>Latte</div>
+              <div className={styles.textGroup}>
+                <div className={styles.title}>Latte</div>
                 <p>Administrator</p>
-                <div className={style.email}>
+                <div className={styles.email}>
                   <Icon.Mail size={18} />
                   <p>latte@coffee.com</p>
                 </div>
               </div>
             </div>
-            <div className={style.detail}>
+            <div className={styles.detail}>
               {details.map((detail) => (
-                <div key={detail.id} className={style.item}>
-                  <div className={style.icon}>{detail.icon}</div>
-                  <div className={style.text}>
+                <div key={detail.id} className={styles.item}>
+                  <div className={styles.icon}>{detail.icon}</div>
+                  <div className={styles.text}>
                     <p>{detail.text}</p>
-                    <p className={style.subtext}>{detail.subtext}</p>
+                    <p className={styles.subtext}>{detail.subtext}</p>
                   </div>
                 </div>
               ))}
             </div>
-            <button className={style.button}>Sign Out</button>
+            <button className={styles.button}>Sign Out</button>
           </div>
         </>
       ) : (
